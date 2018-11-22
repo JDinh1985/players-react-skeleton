0d877586dc06a0ae752d79a37d727c893e513d3c
Joseph Dinh <JDinh1985@gmail.com>
2018-11-22 16:25:29 -0500

    install and configure jest and enzyme.

---
+++ tests/unit/Home.spec.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Home from '../../src/components/Home/Home';
+
+// test('it should add 1 + 2 equals 3', () => {
+//   expect(Home(1, 2)).toBe(3);
+// });
+
+describe('Home component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Home />);
+  });
+
+  it('should render the component without crashing', () => {
+    expect(wrapper).toBeTruthy();
+  });
+
+  it('should have 2 buttons', () => {
+    const buttons = wrapper.find('button');
+    expect(buttons.length).toEqual(2);
+  });
+
+  it('should add 2+2 = 4', () => {
+    expect(2 + 2).toEqual(4);
+  });
+});
